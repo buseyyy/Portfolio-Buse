f7807c23493ada86c2786ef27f402b2018b85bd6
Buse Yılmaz <busey229@gmail.com>
2025-12-16 22:56:13 +0300

    Menu removed on mobile

--- assets/js/nav.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.navlinks');
-
-hamburger.addEventListener('click', () => {
-  navLinks.classList.toggle('active'); // Menü aç/kapa
-});
